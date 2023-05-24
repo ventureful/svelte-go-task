171be2920cd3a4a96276c2403a90241962c215ec
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-05-24 23:47:02 +0200

    update

--- client/src/routes/AnimeComponent.svelte
+++ client/src/routes/AnimeComponent.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-
     export let title: string;
     export let mal_id: number;
     export let image: string;
 </script>
 
 <h1>{title}</h1>
+
+
+<a href="/{mal_id}">
 <img
     class="hover:opacity-50 hover:cursor-pointer"
     src={image}
     alt={title}
-    on:click={() => goto(`/${mal_id}`)}
     height="200px"
     width="200px"
 />
+</a>
